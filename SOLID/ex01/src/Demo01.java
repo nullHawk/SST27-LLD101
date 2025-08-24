a24d1b4dc2d172baec069bf469e9328c3e6b2a0f
Suryansh <surushakya@proton.me>
2025-08-24 18:01:03 +0000

    fix: Dependency Injection

--- SOLID/ex01/src/Demo01.java
+++ SOLID/ex01/src/Demo01.java
@@ -2,6 +2,8 @@
 
 public class Demo01 {
     public static void main(String[] args) {
-        new OrderService().checkout("a@shop.com", 100.0);
+        IEmailClient emailClient = new EmailClient();
+        OrderService orderService = new OrderService(emailClient); // Injecting interface to OrderService earlier OrderService class was directly creating EmailClient instance which violates Dependency Inversion Principle
+        orderService.checkout("a@shop.com", 100.0);
     }
 }
